bc3a2d47e7e167cd2f4671bd6c94bbafc61d22ff
Prashant prabhakar <prashantp.cs.21@nitj.ac.in>
2023-06-20 17:38:46 +0530

    moved the assets folder to public

--- src/PageNotFound404.jsx
+++ src/PageNotFound404.jsx
@@ -5,7 +5,7 @@ export default function NotFound() {
     return (
         <div className="not-found-container">
             <h1>This page was not found , but u can stay and cry with me <span><img src="src\assets\people-hugging_1fac2.png" alt="" width={40}/></span> </h1>
-            <img src="src\assets\404error.jpg" alt="" height={400}/>
+            <img src="\assets\404error.jpg" alt="" height={400}/>
             <Link to="/" className="link-button">Return to Home please!!</Link>
         </div>
     )
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./About.css";
-import bgImg from "../assets/bg-about.png";
+import bgImg from "/assets/bg-about.png";
 import { Link } from "react-router-dom";
 
 export default function About() {
